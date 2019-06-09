e1fb5bb58643d3b582b6113e0d7d3ce4b0b1039d
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-06-09 16:04:30 -0400

    html routes file completed

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,17 +4,22 @@ var path = require("path");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(_dirname,""))
-    });
+    res.sendFile(path.join(_dirname, "../html/index.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load Sign Up/Account Page
+  app.get("/accounts", function(req, res) {
+    res.sendFile(path.join(_dirname, "../html/signup.html"));
+  });
+
+  // Load Sign Up/Account Page
+  app.get("/search", function(req, res) {
+    res.sendFile(path.join(_dirname, "../html/search.html"));
+  }); 
+  
+  // Load Sign Up/Account Page
+  app.get("/messages", function(req, res) {
+    res.sendFile(path.join(_dirname, "../html/messages.html"));
   });
 
   // Render 404 page for any unmatched routes
