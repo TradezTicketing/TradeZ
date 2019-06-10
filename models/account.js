69ba7979db24e678895edf621c1a5585b40972b6
Greg Baker <semperfifcd@gmail.com>
2019-06-10 19:43:56 -0400

    Baseline models complete with association, still needed flags, validations, and defaults.

---
+++ models/account.js
@@ -0,0 +1,21 @@
+module.exports = function (sequelize, DataTypes) {
+  var Account = sequelize.define("Account", {
+    id: {
+      // Properties
+      type: DataTypes.INTEGER,
+      // Flags
+      allowNull: false,
+      // Validations
+      validate: {
+
+      }
+    },
+    username: DataTypes.STRING,
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    zipcode: DataTypes.INTEGER,
+    prefereddistance: DataTypes.INTEGER,
+    password: DataTypes.STRING
+  });
+  return Account;
+};
