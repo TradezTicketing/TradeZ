7981e6d0b05a9d65984fff0fa898e2fae4bf4bc8
Veronique Conus <veronique@veroniques-mbp.home>
2019-06-11 02:13:10 -0400

    added the additional underscores to the dirnames and switched sendFiles to render for myaccount and index

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ var path = require("path");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(_dirname, "../html/index.html"));
+    res.render("index", {});
   });
 
   // Load Sign Up
@@ -12,14 +12,19 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../html/signup.html"));
   });
 
-  // Load Sign Up/Account Page
+   // Load Account Page in handlebars, we need to pass in the email object here!
+   app.get("/myaccount", function(req, res) {
+    res.render("myaccount", {});
+  });
+
+  // Search
   app.get("/search", function(req, res) {
-    res.sendFile(path.join(_dirname, "../html/search.html"));
+    res.sendFile(path.join(__dirname, "../html/search.html"));
   }); 
   
   // Load Sign Up/Account Page
   app.get("/messages", function(req, res) {
-    res.sendFile(path.join(_dirname, "../html/messages.html"));
+    res.sendFile(path.join(__dirname, "../html/messages.html"));
   });
 
   // Render 404 page for any unmatched routes
