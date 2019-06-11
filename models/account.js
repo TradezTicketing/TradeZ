a4f8c6c64c386d503dab460ff905732cbc69d0bf
Greg Baker <semperfifcd@gmail.com>
2019-06-10 20:26:41 -0400

    Adjusted transactions table and added comments for explination.

--- models/account.js
+++ models/account.js
@@ -3,6 +3,7 @@ module.exports = function (sequelize, DataTypes) {
     id: {
       // Properties
       type: DataTypes.INTEGER,
+
       // Flags
       allowNull: false,
       // Validations
@@ -10,12 +11,87 @@ module.exports = function (sequelize, DataTypes) {
 
       }
     },
-    username: DataTypes.STRING,
-    firstname: DataTypes.STRING,
-    lastname: DataTypes.STRING,
-    zipcode: DataTypes.INTEGER,
-    prefereddistance: DataTypes.INTEGER,
-    password: DataTypes.STRING
+
+    username: {
+      // Properties
+      type: DataTypes.STRING,
+
+      // Flags
+      allowNull: false,
+      // Validations
+      validate: {
+
+      }
+    },
+
+    firstname: {
+      // Properties
+      type: DataTypes.STRING,
+
+      // Flags
+      allowNull: false,
+      // Validations
+      validate: {
+
+      }
+    },
+
+    lastname: {
+      // Properties
+      type: DataTypes.STRING,
+
+      // Flags
+      allowNull: false,
+      // Validations
+      validate: {
+
+      }
+    },
+
+    zipcode: {
+      // Properties
+      type: DataTypes.INTEGER,
+
+      // Flags
+      allowNull: false,
+      // Validations
+      validate: {
+
+      }
+    },
+
+    prefereddistance: {
+      // Properties
+      type: DataTypes.INTEGER,
+
+      // Flags
+      allowNull: false,
+      // Validations
+      validate: {
+
+      }
+    },
+
+    password: {
+      // Properties
+      type: DataTypes.STRING,
+
+      // Flags
+      allowNull: false,
+      // Validations
+      validate: {
+
+      }
+    },
   });
+
+  Account.associate = function (models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Account.hasMany(models.Transaction, {
+      onDelete: "cascade"
+    });
+  };
+
   return Account;
 };
