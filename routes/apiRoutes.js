c93401ef917562e0fb75aeacb00c8629ff62aba9
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-06-10 20:49:28 -0400

    API Routes, need Search

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,40 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/", function(req, res) {
-    db.Accounts.findAll({}).then(function(db) {
-      res.json(db);
-    });
-  });
-  app.get("/api/", function(req, res) {
-    db.Accounts.findAll({}).then(function(db) {
+  // Get all experiences the account holder has to offer
+  app.get("/api/transaction/:id", function(req, res) {
+    db.Transaction.findAll({
+      where: {
+        AccountId: req.param.id
+      }
+    }).then(function(db) {
       res.json(db);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  //Get all existing experiences to list in what i want drop down
+app.get("/api/experiences"), function(req,res){
+  db.Transactions.findAll({}).then(function(db){
+    res.json(db)
   });
+}
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Search
+  // app.get("/api/examples", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
+
+  // Delete an experience by id
+  app.delete("/api/deletetransaction/:id", function(req, res) {
+    db.Transaction.destroy({ where: { id: req.params.id } }).then(function(
+      db
+    ) {
+      res.json(db);
     });
   });
 };
+
+
+//Create Entry into Database from Account page
