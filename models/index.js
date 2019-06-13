dc93ea90eea801a1a08e84d35161baae848e9b9c
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-06-12 22:03:48 -0400

    trying to link remote database

--- models/index.js
+++ models/index.js
@@ -4,8 +4,8 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
+var env = process.env.NODE_ENV || "production";
+var config = require("../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
@@ -15,9 +15,10 @@ if (config.use_env_variable) {
     config.database,
     config.username,
     config.password,
-    config
+    config.dialect
   );
 }
+
 
 fs.readdirSync(__dirname)
   .filter(function(file) {
